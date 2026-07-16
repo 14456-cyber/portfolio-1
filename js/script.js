6c44c78c6ceb29c4db3f61c657da119d4fada6a5
JIRAORN01 <14456@phanthong.ac.th>
2026-07-16 15:06:46 +0700

    Implement 'Back to Top' button functionality
    Add functionality for 'Back to Top' button on scroll.

--- js/script.js
+++ js/script.js
@@ -31,3 +31,34 @@ const observer = new IntersectionObserver((entries) => {
 cards.forEach(card=>{
     observer.observe(card);
 });
+// ====================
+// Back To Top
+// ====================
+
+const topBtn=document.getElementById("topBtn");
+
+window.onscroll=function(){
+
+if(document.body.scrollTop>300||document.documentElement.scrollTop>300){
+
+topBtn.style.display="block";
+
+}else{
+
+topBtn.style.display="none";
+
+}
+
+}
+
+topBtn.onclick=function(){
+
+window.scrollTo({
+
+top:0,
+
+behavior:"smooth"
+
+});
+
+}
